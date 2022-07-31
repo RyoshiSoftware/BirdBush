7c50423330d08f239e70909cdf190364ad1ef360
Hoshinohrian <90474813+Hoshinohrian@users.noreply.github.com>
2022-07-31 15:23:01 -0500

    Added a timer for spawning the birds

--- Assets/Scripts/Target/TargetSpawner.cs
+++ Assets/Scripts/Target/TargetSpawner.cs
@@ -8,17 +8,36 @@ public class TargetSpawner : MonoBehaviour
 {
 
     [SerializeField] private GameObject target;
-    [SerializeField] private int targetCap;
+    [SerializeField] private int targetCap = 3;
+    [SerializeField] private float timeRemaining = 3;
     [field: SerializeField] public TargetWaypointManager waypointManager { get; private set; }
     private GameObject targetClone;
+    private int targetCount = 0;
 
     void Start()
     {
-        SpawnTarget();
+        timeRemaining = UnityEngine.Random.Range(3f, 10f);
+    }
+
+    private void Update() 
+    {
+        if (targetCount < targetCap)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                SpawnTarget();
+                timeRemaining = UnityEngine.Random.Range(3f, 10f);;
+            }
+        }
     }
 
     private void SpawnTarget()
     {
+        targetCount ++;
         targetClone = Instantiate(target, this.transform.position, this.transform.rotation);
         targetClone.transform.parent = this.transform;
     }
